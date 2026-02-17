e2acd54b15c53e205992f32020414c3d64785a89
Codar-Boy <103827533+Codar-Boy@users.noreply.github.com>
2026-02-18 00:24:22 +0530

    Update pdf_book.js

--- pdf_book.js
+++ pdf_book.js
@@ -3,34 +3,34 @@ const flipSound = document.getElementById("pageSound");
 
 const pagesContent = [
 
-`<div class="gold-text">শ্রীশ্রী গণেশায় নমঃ</div>`,
+`<div class="subtitle">শ্রীশ্রী গণেশায় নমঃ</div>`,
 
 `<div>
-  <div class="gold-text">কাজল ❤️ পূজা</div>
-  <div class="couple-wrapper" style="margin-top:25px;">
+  <div class="couple-name">কাজল ❤️ পূজা</div>
+  <div class="couple-wrapper">
     <img src="couple.png" class="couple-img">
   </div>
 </div>`,
 
-`<div class="gold-text">
+`<div class="subtitle">
 শুভ বিবাহ<br>
 ০৯ই মার্চ ২০২৬<br>
 সাহেবেরহাট, বড় নলাঙ্গি বাড়ি, কোচবিহার
 </div>`,
 
-`<div class="gold-text">
+`<div class="subtitle">
 রিসেপশন<br>
 ১২ই মার্চ ২০২৬<br>
 সাহেবেরহাট, বড় নলাঙ্গি বাড়ি, কোচবিহার
 </div>`,
 
-`<div class="gold-text">
+`<div class="subtitle">
 আমাদের গল্প<br><br>
 এক নীরব বিকেলের আলোয় শুরু হয়েছিল তাদের পথচলা।<br>
 আজ সেই ভালোবাসা পেয়েছে পবিত্র অঙ্গীকার।
 </div>`,
 
-`<div class="gold-text">আপনার উপস্থিতি ও আশীর্বাদ একান্ত কাম্য</div>`
+`<div class="subtitle">আপনার উপস্থিতি ও আশীর্বাদ একান্ত কাম্য</div>`
 
 ];
 
