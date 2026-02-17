d531eb63e0c5f0949d6054122ee81f6570917bbf
Codar-Boy <103827533+Codar-Boy@users.noreply.github.com>
2026-02-18 00:04:50 +0530

    Update pdf_book.js

--- pdf_book.js
+++ pdf_book.js
@@ -7,26 +7,31 @@ const pagesContent = [
 `<h3 class="gold">শ্রীশ্রী গণেশায় নমঃ</h3>`,
 
 `<div style="text-align:center;">
+  <h2 class="gold">কাজল ❤️ পূজা</h2>
 
-<h2 class="gold">কাজল ❤️ পূজা</h2>
+  <div style="margin-top:20px; display:flex; justify-content:center;">
+    <img
+      src="https://cdn.iconscout.com/illustration/premium/thumb/bengali-mariage-couple-4467548-3703815.png"
+      alt="Bengali Marriage Couple"
+      class="couple-img"
+    />
+  </div>
+</div>`,
 
-<div style="margin-top:20px; display:flex; justify-content:center; align-items:center;">
+`<h3 class="gold">
+শুভ বিবাহ<br>
+০৯ই মার্চ ২০২৬<br>
+সাহেবেরহাট, বড় নলাঙ্গি বাড়ি, কোচবিহার
+</h3>`,
 
-  <img
-    src="https://cdn.iconscout.com/illustration/premium/thumb/bengali-mariage-couple-4467548-3703815.png"
-    alt="Bengali Marriage Couple"
-    style="max-width:280px; width:90%; animation:coupleFade 1.8s ease-in-out;"
-  />
+`<h3 class="gold">
+রিসেপশন<br>
+১২ই মার্চ ২০২৬<br>
+সাহেবেরহাট, বড় নলাঙ্গি বাড়ি, কোচবিহার
+</h3>`,
 
-</div>
-
-</div>`
-
-`<h3 class="gold">শুভ বিবাহ<br>০৯ই মার্চ ২০২৬<br>সাহেবেরহাট, বড় নালাঙ্গি বাড়ি, কোচবিহার</h3>`,
-
-`<h3 class="gold">রিসেপশন<br>১২ই মার্চ ২০২৬<br>সাহেবেরহাট, বড় নালাঙ্গি বাড়ি, কোচবিহার</h3>`,
-
-`<h3 class="gold">আমাদের গল্প<br><br>
+`<h3 class="gold">
+আমাদের গল্প<br><br>
 এক নীরব বিকেলের আলোয় শুরু হয়েছিল তাদের পথচলা।<br>
 আজ সেই ভালোবাসা পেয়েছে পবিত্র অঙ্গীকার।
 </h3>`,
@@ -42,10 +47,10 @@ function renderBook(){
     page.style.zIndex=100-index;
     page.innerHTML=html;
 
-    page.onclick = () => {
-    page.classList.toggle("flipped");
-    flipSound.currentTime = 0;
-    flipSound.play().catch(() => {});
+    page.onclick=()=>{
+      page.classList.toggle("flipped");
+      flipSound.currentTime=0;
+      flipSound.play().catch(()=>{});
     };
 
     book.appendChild(page);
@@ -68,6 +73,3 @@ async function exportPDF(){
 
   pdf.save("Wedding_Invitation.pdf");
 }
-
-
-
